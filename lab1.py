b2b06df28f5f09e6b926b18ca6b38903601fbc22
Chris-J-Holden <114409715+Chris-J-Holden@users.noreply.github.com>
2022-09-26 14:20:56 +0100

    Add files via upload

---
+++ lab1.py
@@ -0,0 +1,29 @@
+# Practical
+print("the program has executed")
+
+print("msg1")
+print("msg2")
+print("msg3")
+
+help()
+keywords
+
+help(input)
+
+quit()
+
+# Exercises
+
+# Portfolio
+#-----------
+# error = "error, incorrect answer"
+name = input("What is your name");
+print("hello " + name);
+
+tempc = (float(input("Temperatutre in degree celsius: ")));
+tempf = (tempc*9/5)+32;
+
+# if tempf != float:
+#   print(error)
+# else:
+print(str(tempf) + " degrees Fahreneit");
